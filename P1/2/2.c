29a9dc82be4a61e525e330e616f0160776ed613d
Paddy <ipaddy.lutz@gmail.com>
2020-11-26 17:41:22 +0100

    vergleichen und nicht gleich true setzen

--- P1/2/2.c
+++ P1/2/2.c
@@ -40,7 +40,7 @@ int main() {
     printf("Weapons in inventory:\n");
 
     for (int i = 0; i < 5; i++) { // 1. runtime array out of bounds with i=5
-        if (weapons[i]->in_inventory = true) {
+        if (weapons[i]->in_inventory == true) { // 2. runtime error, compare and not is
             printf("%s \n", weapons[i]->name); //4. compiler error, printf need char* datatype
         }
     }
