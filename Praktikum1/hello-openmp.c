775c7f121f171557acff9486981d638b8ed5b865
Patrick Lutz <mail@plutz.xyz>
2020-12-14 11:33:12 +0100

    Warnings im code aufgeraeumt

--- Praktikum1/hello-openmp.c
+++ Praktikum1/hello-openmp.c
@@ -1,18 +1,17 @@
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <omp.h>
 #include <time.h>
 
-int main ( int argc, char *argv[] ) {
+int main(int argc, char *argv[]) {
 
-int n = omp_get_max_threads(void);
-printf("maximum number of threads: %d\n", n);
+  int n = omp_get_max_threads();
+  printf("maximum number of threads: %d\n", n);
 
-srand(time(0));
+  srand(time(0));
 
-void omp_set_num_threads (rand() %n);
+  omp_set_num_threads(rand() % n);
 
-#pragma omp parallel{
-printf("Hello %d\n", omp_get_thread_num());
-}
+#pragma omp parallel
+  printf("Hello %d\n", omp_get_thread_num());
 }
