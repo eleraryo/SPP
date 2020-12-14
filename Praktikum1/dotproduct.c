abf1ab8920f832f7801120f9ebc0c25753d8fec5
Patrick Lutz <mail@plutz.xyz>
2020-12-14 12:42:07 +0100

    aufgabe 3 auf lokaler maschine am laufen

--- Praktikum1/dotproduct.c
+++ Praktikum1/dotproduct.c
@@ -1,16 +1,15 @@
-# include <stdlib.h>
-# include <stdio.h>
-# include <math.h>
-# include <omp.h>
+#include <math.h>
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main ( int argc, char *argv[] );
-double test01 ( int n, double x[], double y[] );
-double test02 ( int n, double x[], double y[] );
+int main(int argc, char *argv[]);
+double test01(int n, double x[], double y[]);
+double test02(int n, double x[], double y[], int threads);
 
 /******************************************************************************/
 
-int main ( int argc, char *argv[] )
-{
+int main(int argc, char *argv[]) {
   double factor;
   int i;
   int n;
@@ -21,55 +20,60 @@ int main ( int argc, char *argv[] )
 
   n = 100;
 
-  while ( n < 1000000 )
-  {
+  while (n < 10000000) {
     n = n * 10;
 
-    x = ( double * ) malloc ( n * sizeof ( double ) );
-    y = ( double * ) malloc ( n * sizeof ( double ) );
+    x = (double *)malloc(n * sizeof(double));
+    y = (double *)malloc(n * sizeof(double));
 
-    factor = ( double ) ( n );
-    factor = 1.0 / sqrt ( 2.0 * factor * factor + 3 * factor + 1.0 );
+    factor = (double)(n);
+    factor = 1.0 / sqrt(2.0 * factor * factor + 3 * factor + 1.0);
 
-    for ( i = 0; i < n; i++ )
-    {
-      x[i] = ( i + 1 ) * factor;
+    for (i = 0; i < n; i++) {
+      x[i] = (i + 1) * factor;
     }
 
-    for ( i = 0; i < n; i++ )
-    {
-      y[i] = ( i + 1 ) * 6 * factor;
+    for (i = 0; i < n; i++) {
+      y[i] = (i + 1) * 6 * factor;
     }
 
-    printf ( "\n" );
-/*
-  Test #1
-*/
-    //...YOU NEED TO FILL HERE ...
-    
-    printf ( "  Sequential  %8d  %14.6e  %15.10f\n", n, xdoty, wtime );
-/*
-  Test #2
-*/
-    //...YOU NEED TO FILL HERE ...
+    printf("\n");
+    /*
+      Test #1
+    */
+    double start = omp_get_wtime();
+    xdoty = test01(n, x, y);
+    double end = omp_get_wtime();
+    wtime = end - start;
 
-    printf ( "  Parallel    %8d  %14.6e  %15.10f\n", n, xdoty, wtime );
-  
-    free ( x );
-    free ( y );
+    printf("  Sequential  %8d  %14.6e  %15.10f\n", n, xdoty, wtime);
+    /*
+      Test #2
+    */
+    for (int j = 1; j <= 16; j *= 2) {
+      double start_p = omp_get_wtime();
+      xdoty = test02(n, x, y, j);
+      double end_p = omp_get_wtime();
+      wtime = end_p - start_p;
+      printf("  Parallel    %8d  %14.6e  %15.10f  threads %8d\n", n, xdoty,
+             wtime, j);
+    }
+
+    free(x);
+    free(y);
   }
-/*
-  Terminate.
-*/
-  printf ( "\n" );
-  printf ( "DOT_PRODUCT\n" );
-  printf ( "  Normal end of execution.\n" );
+  /*
+    Terminate.
+  */
+  printf("\n");
+  printf("DOT_PRODUCT\n");
+  printf("  Normal end of execution.\n");
 
   return 0;
 }
 
-//Sequential version
-double test01 ( int n, double x[], double y[] )
+// Sequential version
+double test01(int n, double x[], double y[])
 
 {
   int i;
@@ -77,23 +81,27 @@ double test01 ( int n, double x[], double y[] )
 
   xdoty = 0.0;
 
-  //...YOU NEED TO FILL HERE...
-
+  for (int i = 0; i < n; i++) {
+    xdoty += x[i] * y[i];
+  }
 
   return xdoty;
 }
 
-//Parallel version
-double test02 ( int n, double x[], double y[] )
+// Parallel version
+double test02(int n, double x[], double y[], int threads)
 
 {
   int i;
   double xdoty;
 
   xdoty = 0.0;
+  omp_set_num_threads(threads);
 
-  //...YOU NEED TO FILL HERE...
+#pragma omp parallel for reduction(+ : xdoty)
+  for (int i = 0; i < n; i++) {
+    xdoty += x[i] * y[i];
+  }
 
   return xdoty;
 }
-
